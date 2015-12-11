3bbf522f779c7471cbf6d81f219d09fd19a03359
of-el <avelaguilar24@gmail.com>
2015-12-10 18:18:42 -0800

    added datapath and mux

---
+++ CPU_Datapath/Mux.cpp
@@ -0,0 +1,14 @@
+#include "Mux.h"
+
+Mux::Mux()
+{
+
+}
+
+int
+Mux::Choose(int source1, int source2)
+{
+	
+
+	return result;
+}
---
+++ CPU_Datapath/Mux.h
@@ -0,0 +1,14 @@
+#ifndef Mux_H
+#define Mux_H
+
+class Mux
+{
+private:
+	int a, b, result;
+	 
+public:
+	Mux();
+	int Choose(int, int);
+};
+
+#endif Mux_H
