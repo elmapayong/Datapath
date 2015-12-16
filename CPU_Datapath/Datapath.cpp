434ed116a2dcc12e287041d27d5949b59778dc27
elma <elmapayong@gmail.com>
2015-12-16 10:46:39 -0800

    added cout saying which section is being run

--- CPU_Datapath/Datapath.cpp
+++ CPU_Datapath/Datapath.cpp
@@ -15,13 +15,19 @@ void Datapath::Run()
 		cout << endl;
 
 		/*~~~~~~~ RISING CLOCK EDGE ~~~~~~~*/
-
+		cout << "- RISING CLOCK EDGE - " << endl;
+		cout << "Write data into Registers..." << endl;
+		cout << "Update PC..." << endl;
+		cout << "----------------" << endl;
+		cout << "Breaking apart instructions..." << endl;
 		instr_mem.FetchInstruction(PC);
 
+		cout << "Sending PC to adder..." << endl;
 		//incremment PC to next instruction
 		PC_adder.setA(PC);
 		PC_adder.Add();
 
+		cout << "Sending to Control and setting all control lines..." << endl;
 		//setting all the controls/choices
 		control.setOpCode(instr_mem.opcode, instr_mem.funct);
 		inst_mux.setChoiceB(control.RegDst);
@@ -34,6 +40,7 @@ void Datapath::Run()
 		alu_mux.setChoiceB(control.ALUSrc);
 		registers.setRegWrite(control.RegWrite);
 
+		cout << "Figuring out branch line..." << endl;
 		//branch line - update PC 
 		branch_mux.setA(PC_adder.getResult());
 		branch_adder.setA(PC_adder.getResult());
@@ -41,12 +48,14 @@ void Datapath::Run()
 		branch_adder.Add();
 		branch_mux.setB(branch_adder.getResult());
 
+		cout << "Sending and setting up to Registers..." << endl;
 		//set up registers
 		registers.setReadRegisters(instr_mem.rs, instr_mem.rt);
 		inst_mux.setA(instr_mem.rt);
 		inst_mux.setB(instr_mem.rd);
 		registers.setWriteRegister(inst_mux.getResult());
 
+		cout << "Set up ALU and calculate..." << endl;
 		//process ALU
 		alu.setA(registers.getRsData());
 		alu_mux.setA(registers.getRtData());
@@ -56,12 +65,15 @@ void Datapath::Run()
 
 
 		/*~~~~~~~ FALLING CLOCK EDGE ~~~~~~~*/
+		cout << "- FALLING CLOCK EDGE - " << endl;
 
+		cout << "Access Data Memory..." << endl;
 		//data memory access
 		data_mem.setAddress(alu.getResult());
 		data_mem.setWriteData(registers.getRtData());
 		data_mem.readMemory();
 
+		cout << "Send data from memory..." << endl << endl;
 		//send data from memory
 		data_mux.setB(data_mem.getReadData());
 		data_mux.setA(alu.getResult());
@@ -78,7 +90,7 @@ void Datapath::Run()
 		cout << "    END OF CYCLE:" << endl;
 		printAll();
 		cout << "PC at beginning of next cycle: " << PC << endl;	//DELETE
-		cout << endl;
+		cout << endl << endl;
 		
 		system("pause");
 	}
